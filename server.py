29ace93c6efebf219c646e58a36538ee3c605feb
Saqib <Splasher733@gmail.com>
2024-06-03 22:11:39 +0500

    final

--- server.py
+++ server.py
@@ -22,11 +22,9 @@ def handle_user_joined(username):
 @socketio.on('audio')
 def handle_audio(blob_data):
     print('Audio received')
-    # Base64 encode the binary data
+    
     encoded_audio = base64.b64encode(blob_data)
-    # Convert the encoded audio bytes to a string for transmission
     encoded_audio_str = encoded_audio.decode('utf-8')
-    # Send the encoded audio string over the socket
     emit('audio', encoded_audio_str, broadcast=True)
 
 if __name__ == '__main__':
